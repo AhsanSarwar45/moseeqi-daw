b6892c6c45e2e4e53307fb86d3f0d07160fa6a52
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-04-30 22:34:40 +0500

    Cleanup TrackSequence.tsx

--- components/studio/TrackSequence.tsx
+++ components/studio/TrackSequence.tsx
@@ -1,18 +1,14 @@
 
-import { HStack, VStack, Box, useTheme, Container } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { Track } from '@Interfaces/Track';
 import Ruler from '@scena/react-ruler';
 import React, { useEffect, useRef, useState } from 'react'
-import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
-import { Resizable, ResizeCallbackData } from 'react-resizable'
-import { Rnd } from 'react-rnd'
 import PartView from './PartView';
 
 interface TrackProps {
     track: Track;
     trackIndex: number;
     setSelected: (index: number) => void;
-    setStopTime: (time: number) => void;
     setPartTime: (trackIndex: number, partIndex: number, startTime: number, stopTime: number) => void;
 
 }
@@ -21,18 +17,22 @@ const TrackSequence = (props: TrackProps) => {
 
     const timeScale = useRef<Ruler>(null);
     const timeScaleMain = useRef<Ruler>(null);
-    const theme = useTheme();
 
     useEffect(() => {
-        console.log(props.track)
+        let timeScaleRefValue: Ruler | null = null;
+        let timeScaleMainRefValue: Ruler | null = null;
+
         window.addEventListener('resize', () => {
             timeScale.current?.resize();
             timeScaleMain.current?.resize();
+
+            timeScaleRefValue = timeScale.current;
+            timeScaleMainRefValue = timeScaleMain.current;
         });
         return () => {
             window.removeEventListener('resize', () => {
-                timeScale.current?.resize();
-                timeScaleMain.current?.resize();
+                timeScaleRefValue?.resize();
+                timeScaleMainRefValue?.resize();
             });
         };
     }, []);
@@ -43,19 +43,17 @@ const TrackSequence = (props: TrackProps) => {
 
         <Box
             height="88px"
-            // bgColor="white"
             width={2000}
             position="relative"
             padding="0px"
             onClick={() => props.setSelected(props.trackIndex)}
-            // onDoubleClick={() => props.setStopTime(props.track.stopTime)}
             borderBottom="1px solid gray"
         >
 
 
             {
                 props.track.parts.map((part, partIndex) => (
-                    <PartView key={partIndex} part={part} trackIndex={props.trackIndex} partIndex={partIndex} setStopTime={props.setStopTime} setPartTime={props.setPartTime} />
+                    <PartView key={partIndex} part={part} trackIndex={props.trackIndex} partIndex={partIndex} setPartTime={props.setPartTime} />
                 ))
             }
 
